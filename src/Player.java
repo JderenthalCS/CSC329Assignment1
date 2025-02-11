22610f0b62eb3b6aae565fa73996da5a6ec0c461
JDerenthalCS <JDerenthalCS@Gmail.com>
2025-02-11 08:19:25 -0500

    initial commit

---
+++ src/Player.java
@@ -0,0 +1,79 @@
+public class Player {
+        String name;
+        int score;
+
+    /**
+     * Default Constructor
+      */
+    public Player(){
+
+    }
+
+    /**
+     * Constructor which takes all member variables as parameters
+     * @param name Player Name
+     * @param score Player Score
+     */
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    /**
+     * Deep copyConstructor
+     * @param other New instance of Player object
+     */
+    public Player(Player other){
+        this.name = other.name;
+        this.score = other.score;
+    }
+
+    /**
+     * createClone method which creates a new Player
+     * @param name Attributes name to new player
+     * @param score Attributes score to new player
+     * @return new Player with attributed name and score
+     */
+    public Player createClone(String name, int score){
+        return new Player(this.name, this.score);
+    }
+
+    /**
+     *
+     * @param player
+     * @return
+     */
+    @Override
+    public boolean equals(Player player){
+        if(player.getName() == player.getName()){
+            if(this.name.equals(player.getName())){
+                    return true;
+        }}
+        if(player.getScore() == player.getScore()){
+            return true;
+        }
+        else{return false;}
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+
+
+}
+
+
